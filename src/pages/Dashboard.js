8ccd6229dc00642a0220653b273f026162067690
Ankit Guha <75986219+ankit11hab@users.noreply.github.com>
2021-12-27 22:26:55 +0530

    API tested

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import { connect } from 'react-redux';
+import axios from 'axios'
 
 const Dashboard = (props) => {
+    const [notes,setNotes] = useState([])
+    useEffect(async () => {
+        const config = {
+            headers: {
+                'Authorization': `Bearer ${JSON.parse(localStorage.getItem('authTokens')).access}`
+            }
+        }
+        const data = await axios.get(
+            'http://localhost:8000/api/notes/',
+            config
+        )
+        console.log(data);
+        setNotes(data.data)
+    }, [])
     return (
         <div>
             <h1>Dashboard Page</h1>
             <h3>Welcome { props.user.username }!</h3>
+            {notes.map((note)=>{
+                console.log(note)
+                return(
+                    <div>{note.body}</div>
+                )
+            })}
         </div>
     )
 }
