801643f6aa288ba34df08623a40acf7333ce508c
ayushavi <ayushavi.123@gmail.com>
2022-01-22 00:54:42 +0530

    Update BasicCard.jsx

--- src/components/BasicCard.jsx
+++ src/components/BasicCard.jsx
@@ -20,23 +20,20 @@ export default function BasicCard() {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
-          Word of the Day
-        </Typography>
         <Typography variant='h5' component='div'>
-          be{bull}nev{bull}o{bull}lent
+          Number of seats available: 10
         </Typography>
-        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-          adjective
+        <br />
+        <Typography variant='h5' component='div'>
+          Number of seats selected: 3
         </Typography>
-        <Typography variant='body2'>
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+        <br />
+        <Typography variant='h5' component='div'>
+          Price: 150
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size='small'>Learn More</Button>
+        <Button size='small'>Proceed to Pay</Button>
       </CardActions>
     </Card>
   );
